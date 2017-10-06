155316a965c226b6744ee0a128ed2c7106557712
Carl Piercy <cpiercy111@gmail.com>
2017-10-06 09:57:14 -0700

    10/6

---
+++ CSYes.java
@@ -0,0 +1,18 @@
+// *******************************************************************
+// File Name:
+//
+// Purpose:
+// *******************************************************************
+ 
+public class CSYes
+{
+   // -------------------------------------------------
+   //  The following main method prints an exciting
+   //  message about computer science
+   // -------------------------------------------------
+   public static void main(String args[]) {
+       System.out.println("Computer Science !!!!");
+       System.out.println("=====================");
+    }
+}
+
---
+++ Simple.java
@@ -0,0 +1,32 @@
+
+// ********************************************
+//  Simple.java
+//
+//  Print a simple message about Java.
+// 1.	simple (Why do you even have to change the name of the file in this case?)
+//	Answer: No, doesn't start with capital letter
+//2.	SimpleProgram
+//	Answer: Yes
+//3.	1Simple
+//	Answer: No, starts with integer
+//4.	_Simple_
+//	Answer: Yes
+//5.	*Simple*
+//	Answer: Yes
+//6.	$123_45
+//	Answer: Yes
+//7.	Simple!
+//	Answer: Yes
+  	    
+// ********************************************
+ 
+public class Simple
+{
+ 
+	public static void main (String[] args)
+	{
+ 
+  	  System.out.println ("Java rocks!!");
+ 
+	}
+}
